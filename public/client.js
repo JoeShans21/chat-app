84d9701da9c6f7aa431abe51a4a99fe498468c15
Joe Shanahan <shans.joe@gmail.com>
2018-04-19 10:27:13 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -14,6 +14,6 @@
             var user = $('#username').val();
             var message = $('#message').val();
             socket.emit('messages', message, user);
-            this.reset();
+            $('input[id=message').val('');
             return false;
          });

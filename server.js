026646d3944f7cf772e4dd9f1cc34cbe634de781
joeshans21 <shans.joe@gmail.com>
2018-04-19 13:58:30 -0400

    your message goes here

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'joeshans21.mysql.pythonanywhere-services.com',
+  user     : 'joeshans21',
+  password : 'ipodgenius',
+  database : 'joeshans21$nodechat'
+});
+
 
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/public/index.html')
@@ -12,7 +20,12 @@ app.use(express.static('public'));
 
 io.on('connection', function(client) {
 	console.log('Client connected...');
-
+  connection.connect();
+  connection.query('SELECT content, author WHERE id=1', function (content, author) {
+    client.emit("thread", content, author);
+    client.broadcast.emit("thread", content, author);
+  });
+  connection.end();
 	client.on('join', function(data) {
 		console.log(data);
 	});

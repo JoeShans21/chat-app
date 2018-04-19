0fc1de94d0fc52c5810b066a94198b06dd8712f5
Joe Shanahan <shans.joe@gmail.com>
2018-04-19 15:55:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'sql9.freemysqlhosting.net',
+  user: 'sql9233631',
+  password: 'nBNPW6SRwc',
+  database: 'sql9233631'
+});
 
 
 app.get('/', function(req, res, next) {
@@ -18,6 +25,7 @@ io.on('connection', function(client) {
 	});
 
 	client.on('messages', function(data, user){
+		connection.query('INSERT INTO messages (content,author) VALUES (?, ?);', data, user);
 		client.emit('thread', data, user);
 		client.broadcast.emit('thread', data, user);
 	});
